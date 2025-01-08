819258dbc38545c2215a93d3f110a5664be5f83d
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2025-01-08 21:52:11 +0000

    python smoke test

---
+++ smoke_test.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy as sc
+from scipy.ndimage import gaussian_filter1d
+from scipy.stats import binom
+from scipy.optimize import bisect
+
+if __name__ == "__main__":
+    print("Testing that libraries are working.")
+    print("Pandas version: ", pd.__version__)
+    print("Numpy version: ", np.__version__)
+    print("Matplotlib version: ", matplotlib.__version__)
+    print("Scipy version: ", sc.__version__)
